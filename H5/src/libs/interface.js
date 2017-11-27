929157e48fcc367f8f3701636ddaa3aaf5f7c903
jefferyE <18855972451@163.com>
2017-11-27 11:12:31 +0800

    s

--- H5/src/libs/interface.js
+++ H5/src/libs/interface.js
@@ -1,4 +1,4 @@
-let util = {
+let interface = {
   '/user/login': '/user/login?', // 用户信息
   '/get/notice': '/get/notice?', // 大厅公告
   '/get/rule/qingtui': '/get/rule/qingtui?', // 清推规则
@@ -6,4 +6,4 @@ let util = {
   '/get/rule/dajiu': '/get/rule/dajiu?', // 大九规则
   '/room/create': '/room/create?' // 创建房间
 }
-export default util
+export default interface
