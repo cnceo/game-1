3a88a2c078a48d18420efff225eb0bdad7d9898b
jefferyE <18855972451@163.com>
2017-11-28 08:00:14 +0800

    s

--- H5/build/dev-server.js
+++ H5/build/dev-server.js
@@ -88,7 +88,7 @@ devMiddleware.waitUntilValid(() => {
       _reject(err)
     }
     process.env.PORT = port
-    var uri = 'http://localhost:' + port
+    var uri = 'http://192.168.50.161:' + port
     console.log('> Listening at ' + uri + '\n')
     // when env is testing, don't need open it
     if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
