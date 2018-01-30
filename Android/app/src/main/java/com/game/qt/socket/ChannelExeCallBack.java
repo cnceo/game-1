c8ef85dec502874e38569b9fda48d14f70a506ae
jefferyE <18855972451@163.com>
2018-01-30 14:04:50 +0800

    集成android原生

--- Android/app/src/main/java/com/game/qt/socket/ChannelExeCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.game.qt.socket;
-
-/**
- * Created by Administrator on 2018/1/20 0020.
- */
-
-public interface ChannelExeCallBack {
-    void channelCallback();
-}
--- Android/app/src/main/java/com/game/qt/socket/NettyCallback.java
+++
@@ -1,9 +0,0 @@
-package com.game.qt.socket;
-
-/**
- * Created by xf on 14/12/2017.
- */
-
-public interface NettyCallback {
-    void responseCallback(ResponseMessage responseMessage);
-}
